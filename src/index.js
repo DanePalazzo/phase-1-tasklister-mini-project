fa1fbd60a60bc6b67f079fab475dc21a9efa3a14
Dane Palazzo <PalazzoDD@comcast.net>
2023-09-14 17:30:53 -0600

    phase-1-tasklister-mini-project complete

--- src/index.js
+++ src/index.js
@@ -5,18 +5,64 @@ document.addEventListener("DOMContentLoaded", () => {
     buildToDo(e.target["new-task-description"].value)
     form.reset()
   })
-});
+
+// function buildToDo(todo){
+//   let p = document.createElement('li')
+//   let btn = document.createElement('button')
+//   let editBtn = document.createElement('button')
+//   btn.textContent = 'x'
+//   editBtn.textContent = 'edit'
+//   p.textContent = `${todo} `
+//   handleDelete(btn)
+//   editTasks(editBtn)
+//   p.appendChild(btn)
+//   p.appendChild(editBtn)
+//   document.querySelector('#tasks').appendChild(p)
+// }
 
 function buildToDo(todo){
-  let p = document.createElement('p')
-  let btn = document.createElement('button')
-  btn.addEventListener('click', handleDelete)
-  btn.textContent = 'x'
-  p.textContent = `${todo} `
-  p.appendChild(btn)
-  document.querySelector('#tasks').appendChild(p)
-}
+    let li = document.createElement('li')
+    let txt = document.createElement('p')
+    let btn = document.createElement('button')
+    let editBtn = document.createElement('button')
+    txt.className = 'txt'
+    btn.textContent = 'x'
+    editBtn.textContent = 'edit'
+    txt.textContent = `${todo} `
+    handleDelete(btn)
+    editTasks(editBtn)
+    li.appendChild(txt)
+    li.appendChild(btn)
+    li.appendChild(editBtn)
+    document.querySelector('#tasks').appendChild(li)
+  }
+
 
 function handleDelete(e){
-  e.target.parentNode.remove()
+    e.addEventListener('click', ()=> e.parentNode.remove())
 }
+
+// function editTasks(edit) {
+//     edit.addEventListener('click', () => {
+//       edit.parentNode.remove()
+//       buildToDo(`${document.getElementById(["new-task-description"]).value} `)
+//       form.reset()
+//     })
+//   }
+
+  
+//   function editTasks(edit) {
+//     edit.addEventListener('click', () => {
+//       edit.parentNode.firstChild.textContent = `${document.getElementById(["new-task-description"]).value} `
+//       form.reset()
+//     })
+//   }
+
+  function editTasks(edit) {
+    edit.addEventListener('click', () => {
+      edit.parentNode.querySelector(['.txt']).textContent = `${document.getElementById(["new-task-description"]).value} `
+      form.reset()
+    })
+  }
+
+});
